faecb768aa662395745130d142e88b2ddfd9235e
taro1025 <game4967@gmail.com>
2021-10-01 00:32:20 +0900

    validation

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -5,11 +5,26 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"errors"
 )
 
 type Page struct {
 	Title string
 	Body []byte
+}
+//template.Must validates
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+			http.NotFound(w, r)
+			return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression.
 }
 
 func (p *Page) save() error {
@@ -28,7 +43,11 @@ func loadPage(title string) (*Page, error) {
 
 //view & controller
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	//title := r.URL.Path[len("/view/"):] no validation
+	title, err := getTitle(w, r)       // validated
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -40,7 +59,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+			return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 			p = &Page{Title: title}
@@ -51,10 +73,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	//title := r.URL.Path[len("/save/"):] no validation
+	title, err := getTitle(w, r)        // validated
+	if err != nil {
+			return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -63,14 +89,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	//t, err := template.ParseFiles(tmpl + ".html")     //Parse template
+	err := templates.ExecuteTemplate(w, tmpl+".html", p) //Execute Tmplate already parsed
 	if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
