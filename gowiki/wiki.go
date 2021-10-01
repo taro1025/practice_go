90d4600b24da5c5741ed5e93c91cd1badffe98ec
taro1025 <game4967@gmail.com>
2021-10-01 10:06:39 +0900

    Finish Writing Web Applications

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -42,12 +42,12 @@ func loadPage(title string) (*Page, error) {
 }
 
 //view & controller
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	//title := r.URL.Path[len("/view/"):] no validation
-	title, err := getTitle(w, r)       // validated
-	if err != nil {
-		return
-	}
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/view/"):] ver1 no validation
+	//title, err := getTitle(w, r)       //ver2 validate title
+	//if err != nil {
+	//	return
+	//}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -58,11 +58,11 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-			return
-	}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title, err := getTitle(w, r) ver1 no validation
+	//if err != nil {              //ver2 validate title
+	//		return
+	//}, title string
 	p, err := loadPage(title)
 	if err != nil {
 			p = &Page{Title: title}
@@ -72,15 +72,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	//title := r.URL.Path[len("/save/"):] no validation
-	title, err := getTitle(w, r)        // validated
-	if err != nil {
-			return
-	}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/save/"):] ver1 no validation
+	//title, err := getTitle(w, r)       //ver2 validate title
+	//if err != nil {
+	//		return
+	//}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err = p.save()
+	err := p.save()
 	if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -97,11 +97,23 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+//Collect title validation among view, edit, save
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+			m := validPath.FindStringSubmatch(r.URL.Path)
+			if m == nil {
+					http.NotFound(w, r)
+					return
+			}
+			fn(w, r, m[2])
+	}
+}
+
 func main() {
 	//route
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	//litsten 8000port
 	log.Fatal(http.ListenAndServe(":8080", nil))
